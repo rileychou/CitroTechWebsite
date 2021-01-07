174536c6c2f416b9d41fd6252688e2243eafb458
Riley Chou <riley33chou@gmail.com>
2021-01-06 23:01:38 -0800

    added links

--- scripts/scroll.js
+++ scripts/scroll.js
@@ -3,7 +3,7 @@
   
     var anchorScrolls = {
       ANCHOR_REGEX: /^#[^ ]+$/,
-      OFFSET_HEIGHT_PX: 100,
+      OFFSET_HEIGHT_PX: 110,
   
       /**
        * Establish events, and fix initial scroll position if a hash is provided.
